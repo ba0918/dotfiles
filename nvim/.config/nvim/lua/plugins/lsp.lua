cc3e9809525694a7636ea38c591278a8d1f08d27
ba0918 <pub.dev.delusions@gmail.com>
2026-08-08 06:58:41 +0900

    nvim: mason の LSP パッケージ名を lspconfig サーバー名から正式名に修正
    - 'bashls' は lspconfig のサーバー名で mason パッケージには無い
      - bash-language-server / css-lsp / html-lsp / lua-language-server に修正
    - 誤った名前で 'Cannot find package' エラーが出て mason.nvim の
      config が失敗する問題を解消

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,16 +1,17 @@
 -- LSP サーバーの追加（extras に含まれない言語）
 -- extras で導入されるサーバーは lazyvim.plugins.extras.lang.* が ensure_installed に追加する。
 -- ここでは extras が存在しない言語（Lua / Shell / HTML / CSS）のサーバーを Mason に自動導入させる。
+-- 注: ensure_installed は mason.nvim のパッケージ名（lspconfig のサーバー名ではない）を指定する
 return {
   {
     "mason-org/mason.nvim",
     opts_extend = { "ensure_installed" },
     opts = {
       ensure_installed = {
-        "bashls",
-        "cssls",
-        "html",
-        "lua_ls",
+        "bash-language-server",
+        "css-lsp",
+        "html-lsp",
+        "lua-language-server",
       },
     },
   },
